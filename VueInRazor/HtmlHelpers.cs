e409474eb6263e18b7129e06d03d18430ac15875
matt.h <matt.huang@sjfood.com>
2023-12-14 09:53:27 +0800

    使用兼容性更强的方式渲染

--- VueInRazor/HtmlHelpers.cs
+++ VueInRazor/HtmlHelpers.cs
@@ -1,27 +1,45 @@
 ﻿using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+using System.Web;
 
 public static class HtmlHelpers
 {
-    private const string ScriptsKey = "DelayedScripts";
-
-    public static IDisposable BeginScripts(this IHtmlHelper helper)
+    public static IDisposable DefineScripts(this IHtmlHelper helper, string blockName = "", bool isScope = true)
     {
-        return new ScriptBlock(helper.ViewContext);
+        var fileName = helper.ViewContext.ExecutingFilePath.Replace(".cshtml", "").Replace("/", "_");
+        if (string.IsNullOrWhiteSpace(blockName))
+        {
+            blockName = fileName;
+        }
+        else if (isScope)
+        {
+            blockName += fileName;
+        }
+        return new ScriptBlock(helper.ViewContext, blockName);
     }
 
-    public static HtmlString PageScripts(this IHtmlHelper helper)
+    public static HtmlString RenderScripts(this IHtmlHelper helper, string blockName = "", bool isScope = true)
     {
-        return new HtmlString(string.Join(Environment.NewLine, GetPageScriptsList(helper.ViewContext.HttpContext)));
+        var fileName = helper.ViewContext.View.Path.Replace(".cshtml", "").Replace("/", "_");
+        if (string.IsNullOrWhiteSpace(blockName))
+        {
+            blockName = fileName;
+        }
+        else if (isScope)
+        {
+            blockName += fileName;
+        }
+        return new HtmlString(string.Join(Environment.NewLine, GetPageScriptsList(helper.ViewContext.HttpContext, blockName)));
     }
 
-    private static List<string> GetPageScriptsList(HttpContext httpContext)
+    private static List<string> GetPageScriptsList(HttpContext httpContext, string blockName)
     {
-        var pageScripts = (List<string>)httpContext.Items[ScriptsKey];
+        var pageScripts = (List<string>)httpContext.Items[blockName];
         if (pageScripts == null)
         {
             pageScripts = new List<string>();
-            httpContext.Items[ScriptsKey] = pageScripts;
+            httpContext.Items[blockName] = pageScripts;
         }
         return pageScripts;
     }
@@ -32,19 +50,34 @@ public static class HtmlHelpers
         private readonly StringWriter _scriptsWriter;
 
         private readonly ViewContext _viewContext;
-
-        public ScriptBlock(ViewContext viewContext)
+        private readonly string _blockName;
+        public ScriptBlock(ViewContext viewContext, string blockName)
         {
             _viewContext = viewContext;
             _originalWriter = _viewContext.Writer;
             _viewContext.Writer = _scriptsWriter = new StringWriter();
+            _blockName = blockName;
         }
 
         public void Dispose()
         {
             _viewContext.Writer = _originalWriter;
-            var pageScripts = GetPageScriptsList(_viewContext.HttpContext);
+            var pageScripts = GetPageScriptsList(_viewContext.HttpContext, _blockName);
             pageScripts.Add(_scriptsWriter.ToString());
         }
+    }
+
+    public static string GetString(this IHtmlContent content)
+    {
+        using (var writer = new System.IO.StringWriter())
+        {
+            content.WriteTo(writer, HtmlEncoder.Default);
+            return writer.ToString();
+        }
+    }
+
+    public static string? GetString(this IHtmlString? content)
+    {
+        return content?.ToHtmlString();
     }
 }
---
+++ VueInRazor/wwwroot/js/main.js
@@ -0,0 +1,29 @@
+﻿const comps = [];
+async function addComponent(axios, defineComponent, componentName, hasBody, hasScript, domUrl, scriptUrl) {
+    if (hasBody.toLowerCase() === 'true') {
+        const domResponse = await axios.get(domUrl);
+        if (domResponse.status == 200 && domResponse.data) {
+            let comp = defineComponent({ name: componentName, template: domResponse.data });
+            if (hasScript.toLowerCase() === 'true') {
+                const scriptResponse = await axios.get(scriptUrl);
+                if (scriptResponse.status == 200 && scriptResponse.data) {
+                    const rootString = `<root>${scriptResponse.data}</root>`;
+                    const domParser = new DOMParser();
+                    var dom = domParser.parseFromString(rootString, "text/html");
+                    var tags = dom.querySelector('root').getElementsByTagName("script");
+                    if (tags && tags.length) {
+                        let innerText = '';
+                        for (const tag of tags) {
+                            innerText += tag.innerText;
+                        }
+                        var newComp = await import(URL.createObjectURL(new Blob([innerText], { type: 'application/javascript' })));
+                        if (newComp && newComp.default) {
+                            comp = defineComponent({ ...newComp.default, name: componentName, template: domResponse.data });
+                        }
+                    }
+                }
+            }
+            return comp;
+        }
+    }
+}
